98e5313772a0d7097b8147d6819771da64b59681
«Svetlana» <desko.nn@gmail.com>
2023-02-08 16:15:24 +0300

    Задача 1

---
+++ test5.java
@@ -0,0 +1,11 @@
+public class test5 {
+    static String str1 = "Java";
+    static String str2 = "Java";
+    static String str3 = "Java";
+
+    public static void main(String[] args) {
+        System.out.println(str1.contains(str2));
+        System.out.println();
+    
+}
+}
